b39460c1805945da140bf58d234443ef193b507c
WillSolow <59705089+WillSolow@users.noreply.github.com>
2020-09-14 10:40:55 -0400

    Update RunDMC.py

--- RunDMC.py
+++ RunDMC.py
@@ -1,32 +1,51 @@
-# Will Solow and Skye Rhomberg
+# Will Solow, Skye Rhomberg
 # CS446 Fall 2020
 # Diffusion Monte Carlo (DMC) Simulation
-# Goal is to approximate Schrodinger Equation for 2 or more atoms
+# Script Style
+# Goal: approximate Schrodinger Equation for 2 or more atoms
 
-# import packages
+# Imports
 import numpy as np
 
-# Initialize Constants
+# Initial Constants
+dt = 10
 
-# time in atomic units
- dt = 10 
- 
-# duration of the simulation
 simLength = 10000
 
-# initial number of walkers in simulation
- nWalkers = 2
- 
-# spring constant for harmonic oscilator
-k = 1.0 
- 
+nWalkers = 2
 
-# Start simulation for given duration
-for i in range(simLength): 
-	
-# Input: Variable x, the distance between two atoms
-# Output: The potential energy in the system
-# Uses the equation for potential energy with position and spring constant
+k = 1.0
+
+# g/mol
+mass = 10
+
+equilibriumPosition = 5
+
+electronMass = 9.10938970000e-28
+avogadro = 6.02213670000e+23
+
+reducedMass = (mass / (avogadro * electronMass)) / 2
+
+walkers = 5 + (np.random.rand(nWalkers) - 0.5)
+print(walkers)
+
+for i in range(simLength/dt):
+    referenceEnergy = np.mean(potentialEnergy(walkers)) + (1.0 - (walkers.shape[0]/nWalkers))/(2.0*dt)
+    propogationLengths = np.random.normal(0,np.sqrt(dt/mass),walkers.shape[0])
+    walkers = walkers + propogationLengths
+    potentialEnergies = potentialEnergy(walkers)
+    thresholds = np.random.rand(walkers.shape[0])
+    probDelete = np.exp(-(potentialEnergies-referenceEnergy)*dt)
+    probReplicate = probDelete - 1
+    toReplicate = probReplicate > threshold
+    toDelete = probDelete > threshold
+
+    remainAfterDelete = np.argwhere(walker*np.invert((potentialEnergies > referenceEnergy)*toDelete))
+    replications = np.argwhere(walkers*(potentialEnergies<referenceEnergy)*toReplicate)
+    noChange = np.argwhere(walkers*(potentialEnergies==referenceEnergy))
+    walkers = np.concatenate(remainAfterDelete,replications,noChange)
+
 def potentialEnergy(x):
-	return .5*k*(x-EquilibriumPosition)**2
-	
+    return .5 * k * (x - equilibriumPosition)**2
+
+print(reducedMass)
