6ecf24d25b9964bd1d0ce6bc188d56ccdb076333
WillSolow <59705089+WillSolow@users.noreply.github.com>
2020-09-17 14:39:55 -0400

    updated comments and whitesapace

--- RunDMC.py
+++ RunDMC.py
@@ -12,10 +12,10 @@ import numpy as np
 dt = 10
 
 # simulation length
-sim_length = 10
+sim_length = 1000
 
 # number of initial walkers
-n_walkers = 100
+n_walkers = 1000
 
 # spring constant
 k = 1.0
@@ -45,89 +45,88 @@ def potential_energy(x):
 	
 # simulation loop
 for i in range(sim_length):
-    # print(f'wlk:{walkers}')
     # calculate the reference energy
     # based on the average of all the potential energies of the system
     # adjusted by a statistical value to account for very large or very small populations of walkers
     reference_energy = np.mean( potential_energy(walkers) ) + (1.0 - (walkers.shape[0] / n_walkers) ) / ( 2.0*dt )
     
+	
+	
     # gets a normal distribution about 0 in the range sqrt(dt/mass) of the atom
-    # recall in the model of a harmonic oscillator, only one mass matters
-    print("prop lengths")
+    # recall in the model of a harmonic oscillator, only the reduced mass matters
+	# add these randomized propogation lengths to each walker
     propogation_lengths = np.random.normal(0, np.sqrt(dt/reduced_mass), walkers.shape[0])
-    print(propogation_lengths)
-    # print(f'pln:{propogation_lengths}')
-    # add the propogation length to the position of the current walkers
-    print(f"\n current walkers ({walkers.shape[0]})")
-    print(walkers)
     walkers = walkers + propogation_lengths
-    # print(f'wlk:{walkers}')
+	
+	
+    
     # calculate the new potential energies of each walker in the system
     # returns an array of floats, one per walker
-    print("\n Prop length change")
-    print(walkers)
     potential_energies = potential_energy(walkers)
-    print("\n PotentialEnergies")
-    print(potential_energies)
-    # print(f'pte:{potential_energies}')
+
     
     # calculates a random in range [0,1) for each walker in the system
+	# used to calculate the chance of a walker being deleted or replicated 
     thresholds = np.random.rand(walkers.shape[0])
-    # print(f'thr:{thresholds}')
+	
+	
+	
     # calculates probability of a walker to be deleted
-    # notice that this is calculated for every walker in the system, regardless of the potential energy of the walker
+    # notice that this is calculated for every walker in the system
+	# regardless of the potential energy of the walker
+	# Notice that this is actually the probability that a walker surives
     prob_delete = np.exp(-(potential_energies-reference_energy)*dt)
-    # print(f'p_d:{prob_delete}')
-    # normalize the above probability to the probability of replication
+
+	# Takes prob_delete and normalizes it to the probability of replication
+    # Notice that in the model these differ by -1
     prob_replicate = prob_delete - 1
-    # print(f'p_r:{prob_replicate}')
-    
-    # calculate which walkers actually have the necessary potential energies to merit deletion or replication
+
+	
+	
+    # calculate which walkers actually have the necessary potential energies to merit deletion or 
+	# Notice that to_delete and to_replicate are equal, but for clarity purposes we separate them
     to_delete = prob_delete > thresholds
-    # print(f't_d:{to_delete}')
     to_replicate = prob_replicate > thresholds
-    # print(f't_r:{to_replicate}')
     
-    # uses the argwhere() function to only collect the non-zero elements of the given array
+	
+	
     # use pointwise multiplication with the walkers array with:
     # (if the potential energy is greater than the reference energy AND the walker has probability deleted)
     # then boolean array should be a 1. Invert this and multiply with walkers to get the non-zero positions of the walkers
     # that should remain_after_delete
-    
     delete_walkers = walkers*np.invert( (potential_energies > reference_energy) * to_delete )
-    # print(f'del:{delete_walkers}')
+	
+	
+	# Truncates a shallow copy of the walkers array to store all the walkers that were not deleted
+	# delete_walkers > 0 is an ndarray of booleans
     remain_after_delete = walkers[delete_walkers > 0]
-    print(f"\nWalkers not deleted ({remain_after_delete.shape[0]}): ")
-    print(remain_after_delete)
-    # print(f'rem:{remain_after_delete}')
-    # print(f'{remain_after_delete.shape}')
-     
-    # Will thinks try the where() function. Check out the documentation and maybe figure out how this condition works?
-    
-    # uses the argwhere() function to collect the non-zero elements of the given array
+
+	
+	
     # (if the potential energy is less than the reference energy AND the walker should be replicated) 
     # then the value in the boolean array should be a 1. Multiplying this by walkers gives the non-zero positions of the walkers
     # that should be replicated
     replicate_walkers = (potential_energies < reference_energy)*to_replicate
     # print(f'repl:{replicate_walkers}')
     replications = walkers[replicate_walkers > 0]
-    print(f"\n Replicated walkers ({replications.shape[0]})")
-    print(replications)
-    # print(f'repld:{replications}')
-    # print(f'{replications.shape}')
-    
-    # getting rid of this line. Notice that the no change ones already appear in the remain_after_delete array, so having this array actually replicates them
-    # noChange = np.argwhere(walkers*(potential_energies==referenceEnergy))
-    #walkers = np.concatenate(remain_after_delete,replications,noChange)
-    
+
+	
+	
     # concatenating the remaining after deletion and replications array gives exactly the walkers that weren't deleted (most of which were replicated)
     # note that if the walker was not replicated, it still appears in the remains after deletion array, effectively encompassing
-    # the else statement in the given psuedocode 
+    # the case where the threshold is equal to the probability of deletion or replication
+	# However, due to the stochastic nature of the system, this is unlikely to happen
     walkers = np.append(remain_after_delete, replications)
-    #print(walkers
-    print("Reference Eneryg: " + str(reference_energy))
+
+	
+	# print refernce energy and walkers at each time step
+	# TODO Graph using Matlib
+    print("Reference Energy: " + str(reference_energy))
     print("Num walkers: " + str(walkers.shape[0]))
     print("\n\n ######################### \n\n")
-    # print(f'walkers:{walkers}')
+
+h=1
+convergence_num = .5*h*np.sqrt(k/reduced_mass)
+print("Convergence num" + str(convergence_num))
 
 
