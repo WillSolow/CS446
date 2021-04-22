a238c81f8d2b6cb2b9dc671178c0c3a926128367
WillSolow <59705089+WillSolow@users.noreply.github.com>
2021-04-22 18:50:15 -0400

    remove outliers update

--- Descendent_Weighting/DMC_rs_graph.py
+++ Descendent_Weighting/DMC_rs_graph.py
@@ -12,7 +12,58 @@ import matplotlib.pyplot as plt
 
 n_bins = 50
 
-def plot_oxy_ang(filename):
+def plot_wave_functions(filename,num_files):
+    total_o = []
+    for i in range(num_files):
+        o1, o2, o3 = oxy_ang(filename+f'_{i}.npy')
+        '''
+        plt.figure(1)
+        plt.hist(o_ang_1,bins=n_bins,density=True)
+        plt.xlabel('Walker Oxygen Bond Angle')
+        plt.ylabel('Density')
+        plt.title(f'Density of Oxygen Angle 1')
+
+        plt.figure(2)
+        plt.hist(o_ang_2,bins=n_bins,density=True)
+        plt.xlabel('Walker Oxygen Bond Angle')
+        plt.ylabel('Density')
+        plt.title(f'Density of Oxygen Angle 2')
+
+        plt.figure(3)
+        plt.hist(o_ang_3,bins=n_bins,density=True)
+        plt.xlabel('Walker Oxygen Bond Angle')
+        plt.ylabel('Density')
+        plt.title(f'Density of Oxygen Angle 3')
+        '''
+        plt.figure(i)
+        total_o = np.concatenate((total_o,o1,o2,o3),axis=0)
+        plt.hist(total_o,bins=n_bins,density=True)
+        plt.xlabel('Walker Oxygen Bond Angle')
+        plt.ylabel('Density')
+        plt.title(f'Wave Function of Oxygen Angles after {i+1} million steps')
+
+    plt.show()
+
+# removes the smallest k and largest k oxygen angles from each angle
+# we could also remove by walker, this might be slightly different but 
+# should equate to almost the same thing
+def rm_outliers(o1,o2,o3,k):
+    o1_ind = np.argsort(o1)
+    print(o1.shape)
+    o1 = o1[o1_ind[k:-k]]
+    print(o1.shape)
+    print('\n\n')
+
+    o2_ind = np.argsort(o2)
+    o2 = o2[o2_ind[k:-k]]
+
+    o3_ind = np.argsort(o3)
+    o3 = o3[o3_ind[k:-k]]
+
+    return o1, o2, o3
+
+
+def oxy_ang(filename):
 
     wave_func_out = np.load(filename,allow_pickle=True)
 
@@ -35,35 +86,15 @@ def plot_oxy_ang(filename):
             (oxy_ln_10*oxy_ln_21))
         o3 = (180/np.pi)*np.arccos(np.sum(-oxy_vec_20*-oxy_vec_21, axis=1) / \
             (oxy_ln_20*oxy_ln_21))
-    
+
+        o1, o2, o3 = rm_outliers(o1, o2, o3)
+
         o_ang_1 = np.concatenate((o_ang_1,o1),axis=0)
         o_ang_2 = np.concatenate((o_ang_2,o2),axis=0)
         o_ang_3 = np.concatenate((o_ang_3,o3),axis=0)
 
-    plt.figure(1)
-    plt.hist(o_ang_1,bins=n_bins,density=True)
-    plt.xlabel('Walker Oxygen Bond Angle')
-    plt.ylabel('Density')
-    plt.title(f'Density of Oxygen Angle 1')
-
-    plt.figure(2)
-    plt.hist(o_ang_2,bins=n_bins,density=True)
-    plt.xlabel('Walker Oxygen Bond Angle')
-    plt.ylabel('Density')
-    plt.title(f'Density of Oxygen Angle 2')
-
-    plt.figure(3)
-    plt.hist(o_ang_3,bins=n_bins,density=True)
-    plt.xlabel('Walker Oxygen Bond Angle')
-    plt.ylabel('Density')
-    plt.title(f'Density of Oxygen Angle 3')
-
-    plt.figure(4)
-    plt.hist(np.concatenate((o_ang_1,o_ang_2,o_ang_3),axis=0),bins=n_bins,density=True)
-    plt.xlabel('Walker Oxygen Bond Angle')
-    plt.ylabel('Density')
-    plt.title('Density of all Oxygen Angles')
-
+    
+    return o_ang_1, o_ang_2, o_ang_3
     
 
 def plot_h_dist(filename):
@@ -102,5 +133,5 @@ def plot_h_dist(filename):
 
 
 if __name__ == '__main__':
-    plot_oxy_ang(sys.argv[1])
-    plot_h_dist(sys.argv[1])
+    plot_wave_functions(sys.argv[1],2)
+    #plot_h_dist(sys.argv[1])
